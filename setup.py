3757c937b326a2860ac36e753ba070fba6201527
blackfly <jjensun@gmail.com>
2020-04-06 21:49:52 +0200

    Bump version: 0.1.0-dev0 → 0.1.0-rc0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='prototorch',
-      version='0.1.0-dev0',
+      version='0.1.0-rc0',
       description='Highly extensible, GPU-supported '
       'Learning Vector Quantization (LVQ) toolbox '
       'built using PyTorch and its nn API.',
