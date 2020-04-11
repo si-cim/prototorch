955661af95878bb1afb71dce9051afb89bc21d81
blackfly <jjensun@gmail.com>
2020-04-11 15:12:53 +0200

    Remove utils import from prototorch/__init__.py

--- prototorch/__init__.py
+++ prototorch/__init__.py
@@ -2,10 +2,9 @@
 
 __version__ = '0.1.1-dev0'
 
-from prototorch import functions, modules, utils
+from prototorch import functions, modules
 
 __all__ = [
     'functions',
     'modules',
-    'utils',
 ]
