c54d14c55e5af19f7e07663fde803dd4da736c23
blackfly <jjensun@gmail.com>
2020-04-11 14:59:11 +0200

    Remove datasets import from prototorch/__init__.py

--- prototorch/__init__.py
+++ prototorch/__init__.py
@@ -2,10 +2,9 @@
 
 __version__ = '0.1.1-dev0'
 
-from prototorch import datasets, functions, modules, utils
+from prototorch import functions, modules, utils
 
 __all__ = [
-    'datasets',
     'functions',
     'modules',
     'utils',
