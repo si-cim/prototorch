4540c8848e4134eda45f1d52d470eae43ca1bf22
Alexander Engelsberger <aengelsb@hs-mittweida.de>
2021-04-23 17:21:52 +0200

    Add neural gas energy function as loss.

--- prototorch/modules/losses.py
+++ prototorch/modules/losses.py
@@ -18,3 +18,23 @@ class GLVQLoss(torch.nn.Module):
         mu = glvq_loss(distances, targets, prototype_labels=plabels)
         batch_loss = self.squashing(mu + self.margin, beta=self.beta)
         return torch.sum(batch_loss, dim=0)
+
+
+class NeuralGasEnergy(torch.nn.Module):
+    def __init__(self, lm):
+        super().__init__()
+        self.lm = lm
+
+    def forward(self, d):
+        order = torch.argsort(d, dim=1)
+        ranks = torch.argsort(order, dim=1)
+        cost = torch.sum(self._nghood_fn(ranks, self.lm) * d)
+
+        return cost, order
+
+    def extra_repr(self):
+        return f"lambda: {self.lm}"
+
+    @staticmethod
+    def _nghood_fn(rankings, lm):
+        return torch.exp(-rankings / lm)
