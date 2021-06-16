3fe7873f4b3bedd9bce3957c670cdbd71a4d3136
Alexander Engelsberger <alexanderengelsberger@gmail.com>
2021-06-16 14:51:10 +0200

    fix(githooks): mypy only checks source now
    reverts changes on docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -68,7 +68,7 @@ master_doc = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-#exclude_patterns = []
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use. Choose from:
 # ["default", "emacs", "friendly", "colorful", "autumn", "murphy", "manni",
@@ -124,20 +124,20 @@ htmlhelp_basename = "protoflowdoc"
 
 # -- Options for LaTeX output ---------------------------------------------
 
-#latex_elements = {
-# The paper size ("letterpaper" or "a4paper").
-#
-# "papersize": "letterpaper",
-# The font size ("10pt", "11pt" or "12pt").
-#
-# "pointsize": "10pt",
-# Additional stuff for the LaTeX preamble.
-#
-# "preamble": "",
-# Latex figure (float) alignment
-#
-# "figure_align": "htbp",
-#}
+latex_elements = {
+    # The paper size ("letterpaper" or "a4paper").
+    #
+    # "papersize": "letterpaper",
+    # The font size ("10pt", "11pt" or "12pt").
+    #
+    # "pointsize": "10pt",
+    # Additional stuff for the LaTeX preamble.
+    #
+    # "preamble": "",
+    # Latex figure (float) alignment
+    #
+    # "figure_align": "htbp",
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
