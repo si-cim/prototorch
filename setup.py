cfe8f3525b6f697efb70e8e0b257d19a35b033c8
Alexander Engelsberger <alexanderengelsberger@gmail.com>
2022-01-10 18:48:08 +0100

    ci: update supported python versions
    supported are 3.7, 3.8 and 3.9.
    
    3.6 had EOL in december 2021.
    3.10 has no pytorch distribution yet.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     url=PROJECT_URL,
     download_url=DOWNLOAD_URL,
     license="MIT",
-    python_requires=">=3.6",
+    python_requires=">=3.7,<3.10",
     install_requires=INSTALL_REQUIRES,
     extras_require={
         "datasets": DATASETS,
