236cbbc4d2a38d366d4fe2e41b52d143141e7dd6
Jensun Ravichandran <jjensun@gmail.com>
2022-03-10 14:45:55 +0100

    feat: add color utils

--- prototorch/utils/__init__.py
+++ prototorch/utils/__init__.py
@@ -1,6 +1,11 @@
-"""ProtoFlow utils module"""
+"""ProtoTorch utils module"""
 
-from .colors import hex_to_rgb, rgb_to_hex
+from .colors import (
+    get_colors,
+    get_legend_handles,
+    hex_to_rgb,
+    rgb_to_hex,
+)
 from .utils import (
     mesh2d,
     parse_data_arg,
